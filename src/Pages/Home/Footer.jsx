5dc37707253ba8f61fdd4ad49efa2ecd4c67146f
suraiya sultana <you@example.com>
2024-07-09 05:54:26 +0600

    added footer

---
+++ src/Pages/Home/Footer.jsx
@@ -0,0 +1,32 @@
+const Footer = () => {
+    return (
+        <div className="mt-10">
+            <footer className="footer bg-base-200 text-base-content p-10">
+  <aside>
+    
+    <p className="text-2xl">
+     College Mastermind
+      <br />
+     
+    </p>
+  </aside>
+ 
+  <nav>
+    <h6 className="footer-title">Company</h6>
+    <a className="link link-hover">About us</a>
+    <a className="link link-hover">Contact</a>
+    <a className="link link-hover">Jobs</a>
+    <a className="link link-hover">Press kit</a>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Legal</h6>
+    <a className="link link-hover">Terms of use</a>
+    <a className="link link-hover">Privacy policy</a>
+    <a className="link link-hover">Cookie policy</a>
+  </nav>
+</footer>
+
+        </div>
+    );
+};
+export default Footer;
