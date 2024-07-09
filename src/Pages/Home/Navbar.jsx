e5dd75a02efb1391fcf0ef938e90da44a8631348
suraiya sultana <you@example.com>
2024-07-09 06:39:09 +0600

    edited navbar

--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
         <li><a>Item 3</a></li>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">Logo</a>
+    <a className="btn btn-ghost text-xl">College Mastermind</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
