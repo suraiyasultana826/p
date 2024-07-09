5c3a1adf780ef1db18d8a1786e95ee899fb75ca5
suraiya sultana <you@example.com>
2024-07-09 06:56:10 +0600

    edited btn

--- src/Pages/Home/BestPac.jsx
+++ src/Pages/Home/BestPac.jsx
@@ -6,7 +6,7 @@ const BestPac = () => {
     <h2 className="card-title justify-center text-2xl">Curious Which is the best package for you?</h2>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis rerum, voluptatem inventore suscipit aperiam sit ratione amet necessitatibus officia sequi velit, quam quos, ullam adipisci non vero illum alias placeat?</p>
     <div className="card-actions justify-center">
-      <button className="btn">Book your strategy session Now</button>
+      <button className="btn text-sm lg:text-xl mx-4">Book your strategy session Now</button>
     </div>
   </div>
 </div>
